1cd58ca9f73004a68f3e7683c73bbf259f8b95aa
josealmeida03-ctrl <jose.almeida03@escola.pr.gov.br>
2026-09-16 13:49:30 -0300

    g

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const BotoesCurtir =document.quersyselectoraLL(".curtir");
+botoescurtir.forEsch(function9(botaocurtir)
+  let curtir=false;
+botaocurtir.{addeventistener("click",curtir);
+    function curtir(){
+ const contador= botaocurtir.queryselector("span);
+    if (cortir===false) {
+         contador.textcontent++;
+          curtiu=true;}
+
 
